dbd3bb612698a6e633a52cba973e22f505ced851
marimeli <marimel.casas@gmail.com>
2019-10-23 10:46:10 -0500

    first commit

---
+++ holamundo.js
@@ -0,0 +1,8 @@
+class HolaMundo {
+    constructor(mensaje) {
+        this.mensaje = mensaje;
+    }
+}
+const hola = new HolaMundo('Hola Mundo desde TypeScript');
+console.log(hola.mensaje);
+console.log('Hola Sublime');
---
+++ holamundo.ts
@@ -0,0 +1,9 @@
+class HolaMundo{
+	constructor(public mensaje: string){	
+	}
+}
+
+const hola = new HolaMundo('Hola Mundo desde TypeScript');
+console.log(hola.mensaje);  
+
+console.log('Hola Sublime');
